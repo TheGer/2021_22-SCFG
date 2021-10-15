613f47afde7067d79f8cf311350b59f6e1cb867d
Gerard Said Pullicino <gerards@mcast.org.mt>
2021-10-15 12:32:03 +0200

    6.2A Lesson 1

--- Assets/Resources/Scripts/objectGenerator.cs
+++ Assets/Resources/Scripts/objectGenerator.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class objectGenerator : MonoBehaviour
 {
+
+    GameObject square;
     // Start is called before the first frame update
     void Start()
     {
-        
+       //1. Load square template from resources
+       square = Resources.Load<GameObject>("Prefabs/Square");
+       //2. Instantiate a square in the MIDDLE of the screen at 0 degree rotation
+       GameObject tempSquare = Instantiate(square,new Vector3(0f,0f),Quaternion.identity); 
+       //3. Set a random colour for the square
+       tempSquare.GetComponent<SpriteRenderer>().color = Random.ColorHSV();
+       //4. Find the coordinates of the edges of the square
+
     }
 
-    // Update is called once per frame
+        // Update is called once per frame
     void Update()
     {
         
