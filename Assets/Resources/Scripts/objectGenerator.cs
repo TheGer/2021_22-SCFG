5493f3a516ec08cf163f2e92f5161fd45731f1e8
Gerard Said Pullicino <gerards@mcast.org.mt>
2021-10-18 16:04:27 +0200

    Added squareparent

--- Assets/Resources/Scripts/objectGenerator.cs
+++ Assets/Resources/Scripts/objectGenerator.cs
@@ -5,19 +5,48 @@ using UnityEngine;
 public class objectGenerator : MonoBehaviour
 {
 
-    GameObject square;
+    //variable declarations
+    string myName="";
+    float temperature = 0.0f;
+    int age = 0;
+
+    GameObject square,squareParent;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello My Name Is Gerry");
-        
-       //1. Load square template from resources
-       square = Resources.Load<GameObject>("Prefabs/Square");
-       //2. Instantiate a square in the MIDDLE of the screen at 0 degree rotation
-       GameObject tempSquare = Instantiate(square,new Vector3(0f,0f),Quaternion.identity); 
-       //3. Set a random colour for the square
-       tempSquare.GetComponent<SpriteRenderer>().color = Random.ColorHSV();
-       //4. Find the coordinates of the edges of the square
+       squareParent = new GameObject();
+       squareParent.name = "Cross-parent";
+       squareParent.transform.position = new Vector3(0f,0f);
+       
+
+       for (int counter = 0; counter < 4; counter++)
+       {
+           Debug.Log(counter); 
+            //1. Load square template from resources
+            square = Resources.Load<GameObject>("Prefabs/Square");
+            //2. Instantiate a square in the MIDDLE of the screen at 0 degree rotation
+            GameObject tempSquare = Instantiate(square,new Vector3(counter,0f),Quaternion.identity); 
+            //3. Set a random colour for the square
+            tempSquare.GetComponent<SpriteRenderer>().color = Random.ColorHSV();
+            
+             GameObject tempSquare2 = Instantiate(square,new Vector3(0f,counter),Quaternion.identity); 
+            //3. Set a random colour for the square
+            tempSquare2.GetComponent<SpriteRenderer>().color = Random.ColorHSV();
+
+            //4 more lines to finish the cross
+             GameObject tempSquare3 = Instantiate(square,new Vector3(0f,-counter),Quaternion.identity); 
+            //3. Set a random colour for the square
+            tempSquare3.GetComponent<SpriteRenderer>().color = Random.ColorHSV();
+
+            GameObject tempSquare4 = Instantiate(square,new Vector3(-counter,0f),Quaternion.identity); 
+            //3. Set a random colour for the square
+            tempSquare.GetComponent<SpriteRenderer>().color = Random.ColorHSV();
+            
+
+
+
+
+        }
 
     }
 
